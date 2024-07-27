5410c55d55680dea9abd06e793cb59458d7d861c
Lara Jane Pedrigal <pedrigallarajane99@gmail.com>
2024-07-28 03:38:19 +0800

    Added a bottom navigation menu and its fragment

---
+++ app/src/main/java/com/example/foodrecipe/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.foodrecipe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+    }
+}
